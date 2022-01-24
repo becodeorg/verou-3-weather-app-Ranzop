cb90535f5190485d09489c06a1889e9d2c64eb31
Angelgroenen <bm.bracke.michael@gmail.com>
2022-01-24 12:31:09 +0100

    WIP JSON

--- README.md
+++ README.md
@@ -10,3 +10,15 @@ Display a line graph of temperature over time using a library such as Chart.js
 Remember the user choice on subsequent visits
 Allow the user to compare the weather in two cities
 Use the API of https://unsplash.com/ to show a photo of the city they entered in the form.
+
+
+You call an api using a URL:
+
+api.openweathermap.org/data/2.5/weather?q=Brussels&appid=cbc16f0f5aa36c366d6f598260b41240;
+
+You can query it further using a structure like & _ =
+
+api.openweathermap.org/data/2.5/weather?q=Brussels &time=14h51 &appid=insertkeyhere;
+
+
+You now have .json data; i.e. a bunch of data set into an array.
--- script.js
+++ script.js
@@ -8,10 +8,10 @@ axios ("url")
 .then (response => response.json)
 .then (data => console.log(data)); */
 
-const { default: axios } = require("axios");
+/* const { default: axios } = require("axios");
 
 
-axios.get("https://pokeapi.co/api/v2/ability").then (response => console.log (response));
+axios.get("https://pokeapi.co/api/v2/ability").then (response => console.log (response)); */
 
 
 
@@ -23,6 +23,34 @@ you will want to store that also in the API url, in its appropriate place {city 
 // TODO: submit or enter clickevent -> make weather appear
 // TODO: responsiveness
 
-const config = require("./config")
+/* const config = import("./config"); */
 
 // API + config.key
+
+/* const createWeather = document.createElement("div");
+createWeather.setAttribute("id", "getWeather");
+document.body.appendChild(createWeather);
+
+const getWeather = document.getElementById("getWeather");
+ */
+let weather;
+/* const axios = require('axios').default; */
+
+function setup () {
+    createCanvas (400, 200);
+    loadJSON("http://api.openweathermap.org/data/2.5/weather?q=Brussels&units=metric&appid=cbc16f0f5aa36c366d6f598260b41240", gotData);
+}
+
+function gotData(data) {
+    println (data);
+    weather = data;
+}
+
+function draw() {
+    background(0);
+    if (weather) {
+        ellipse (100, 100, weather.main.temp, weather.main.temp)
+        ellipse (300, 100, weather.main.humidity, weather.main.humidity)
+    }
+}
+
