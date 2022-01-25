ce067003689cd2077fd088743899ace46ddbb7b3
Angelgroenen <bm.bracke.michael@gmail.com>
2022-01-25 11:47:03 +0100

    Succesful API Call

--- script.js
+++ script.js
@@ -8,26 +8,89 @@ axios ("url")
 .then (response => response.json)
 .then (data => console.log(data)); */
 
-import Data from "./config.js";
 
-console.log(Data.key);  
+import Key from "./config.js";
 
 
+  // Retrieving data from JSON
+
+/* getTemp = (data) => {
+  let x =  data.main;
+  console.log (x);
+} */
 
 /* console.log ('yo');
 const test = Data.key;
 console.log (test); */
 
+let submitButton = document.createElement("input");
+submitButton.setAttribute("type", "button");
+submitButton.setAttribute("id", "submit");
+document.body.appendChild(submitButton);
 
-fetch("http://api.openweathermap.org/data/2.5/weather?q=Brussels&units=metric&appid=" + Data.key)
-.then(result => result.json())
-.then(data => console.log (data))
+
+let locationInputValue = document.createElement("input");
+locationInputValue.setAttribute("type", "text");
+locationInputValue.setAttribute("id", "location");
+document.body.appendChild(locationInputValue);
+
+console.log(locationInputValue);
+
+let tempOutput = document.createElement("div");
+tempOutput.setAttribute("id", "temp");
+document.body.appendChild(tempOutput);
+tempOutput.classList.add("temp");
+
+const api_url = "https://api.openweathermap.org/data/2.5/weather?q="
+
+const callWeather = (event) => {
+    event.preventDefault();
+    const location = locationInputValue.value;
+    console.log(location);
+    
+    const getWeather = async () => {
+    const result = await fetch (api_url + location + "&units=metric&appid=" + Key.key);
+    const data = await result.json();
+        console.log(data);
+    }
+    getWeather ();
+}
+
+console.log(api_url + "Brussels" + "&units=metric&appid=" + Key.key);
 
 
-let createTemp = document.createElement("div");
-createTemp.setAttribute("id", "temp");
-document.body.appendChild(createTemp);
-createTemp.classList.add("temp");
+/* getWeather (); */
+
+submitButton.addEventListener('click', callWeather); 
+
+
+console.log(location);
+
+ //   http://api.openweathermap.org/data/2.5/weather?q=
+console.log (api_url + location + "s&units=metric&appid=" + Key.key);
+
+
+
+//async function getWeather () {fetch(api_url + `${location}&units=metric&appid=` + Key.key)
+
+
+
+
+
+
+
+
+
+
+
+
+// .catch method can 'catch' errors that go wrong in the whole fetching process.
+
+// you can use async and await, they only function together.
+
+
+
+
 
 /* const { default: axios } = require("axios");
 
